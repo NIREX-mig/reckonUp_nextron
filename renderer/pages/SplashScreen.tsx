05fea1284f607113be25d283654c6d646830a552
nirex <akay93796@gmail.com>
2025-03-09 14:18:33 +0530

    add: splashScreen

--- renderer/pages/SplashScreen.tsx
+++ renderer/pages/SplashScreen.tsx
@@ -1,40 +1,32 @@
 import { useEffect, useState } from "react";
 import splash from "../public/splash.png";
 import Image from "next/image";
+import { version } from "os";
 
 export default function SplashScreen() {
-  const [visible, setVisible] = useState(true);
   const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    // Simulate loading time (e.g., fetching data, initializing)
-    setTimeout(() => {
-      setVisible(false);
-    }, 3000); // Show splash screen for 3 seconds
-  }, []);
-
+  let version = "V1.0.2";
   useEffect(() => {
     const interval = setInterval(() => {
       setProgress((oldProgress) => {
-        const newProgress = oldProgress + 3;
+        const newProgress = oldProgress + 2;
         if (newProgress >= 100) {
           clearInterval(interval);
         }
         return newProgress;
       });
-    }, 50); // Increase progress every 300ms
+    }, 80); // Increase progress every 80ms
 
     return () => clearInterval(interval);
   }, []);
 
-  if (!visible) return null;
-
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-primary-50 text-xl">
-      <Image src={splash} alt="app_logo" width={180} height={180} />
-      <div className="min-w-[80%] bg-gray-200 h-2 mx-auto mt-8">
+      <p className="absolute right-3 top-3 font-bold text-base">{version}</p>
+      <Image src={splash} alt="app_logo" width={180} height={180} unoptimized />
+      <div className="min-w-[90%] bg-gray-200 h-2 mx-auto mt-8">
         <div
-          className="bg-purple-900 h-2"
+          className={`bg-purple-900 h-2 transition-all duration-[3000]`}
           style={{ width: `${progress}%` }}
         ></div>
       </div>
